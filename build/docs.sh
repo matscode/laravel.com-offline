6ac8bfe77e8798b19d0712db4578adf88e757e1d
Taylor Otwell <taylorotwell@gmail.com>
2014-09-22 10:42:54 -0500

    update docs.

--- build/docs.sh
+++ build/docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/forge/laravel.com/docs/4.0 && git pull
-cd /home/forge/laravel.com/docs/4.1 && git pull
-cd /home/forge/laravel.com/docs/4.2 && git pull
-cd /home/forge/laravel.com/docs/master && git pull
+cd /home/forge/laravel.com/resources/docs/4.0 && git pull
+cd /home/forge/laravel.com/resources/docs/4.1 && git pull
+cd /home/forge/laravel.com/resources/docs/4.2 && git pull
+cd /home/forge/laravel.com/resources/docs/master && git pull
