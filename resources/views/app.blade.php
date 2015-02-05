ef159ecbf69bef79978cac950b09c9be31fbe4fb
Taylor Otwell <taylorotwell@gmail.com>
2015-02-05 11:03:20 -0600

    GA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,5 +49,13 @@
 		<p class="less-significant"><a href="http://jackmcdade.com">Design by Jack McDade</a></p>
 	</footer>
 	<script src="/assets/js/laravel.js"></script>
+    <script>
+        var _gaq=[['_setAccount','UA-23865777-1'],['_trackPageview']];
+        (function(d,t){
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)
+        }(document,'script'));
+    </script>
 </body>
 </html>
