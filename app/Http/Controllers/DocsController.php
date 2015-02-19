4fa86b7efeac9ccbb2997ceb9027eeefc574ef82
Kayla Daniels <kayladnls@gmail.com>
2015-02-19 11:53:47 -0500

    reduced some duplication

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -65,7 +65,7 @@ class DocsController extends Controller {
 	 */
 	protected function isVersion($version)
 	{
-		return in_array($version, ['master', '5.0', '4.2', '4.1', '4.0']);
+		return in_array($version, array_keys($this->getDocVersions()));
 	}
 
 	/**
