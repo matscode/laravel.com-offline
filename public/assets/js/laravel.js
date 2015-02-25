7716ed93a3290524aec8350321697da781e008fc
Matt Stauffer <matt@tighten.co>
2015-02-24 23:55:04 -0500

    Revert breaking jquery change to docs headers.

--- public/assets/js/laravel.js
+++ public/assets/js/laravel.js
@@ -964,7 +964,7 @@ $(function() {
 
   // gheading links
   $('.docs-wrapper').find('a[name]').each(function () {
-        var anchor = $('a[href="#' + this.name + '"]');
+        var anchor = $('<a href="#' + this.name + '"/>');
         $(this).parent().next('h2').wrapInner(anchor);
     })
 
