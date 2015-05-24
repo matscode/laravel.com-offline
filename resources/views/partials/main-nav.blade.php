ee35c3acad33ae94520fc5369816d64e87bdc5b6
Taylor Otwell <taylorotwell@gmail.com>
2015-05-23 21:56:17 -0500

    play with navigation

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,9 +1,14 @@
 <li class="nav-docs"><a href="/docs">Documentation</a></li>
 <li class="nav-laracasts"><a href="https://laracasts.com">Laracasts</a></li>
-<li class="nav-lumen"><a href="http://lumen.laravel.com">Lumen</a></li>
-<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
-<li class="nav-envoyer"><a href="https://envoyer.io">Envoyer</a></li>
-<li class="nav-api"><a href="/api/{{ isset($currentVersion) ? $currentVersion : DEFAULT_VERSION }}">API</a></li>
+
+<li class="dropdown community-dropdown">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Services <span class="caret"></span></a>
+	<ul class="dropdown-menu" role="menu">
+		<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
+		<li class="nav-envoyer"><a href="https://envoyer.io">Envoyer</a></li>
+	</ul>
+</li>
+
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Conference <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
@@ -11,20 +16,17 @@
 		<li><a href="http://laracon.eu">Laracon EU</a></li>
 	</ul>
 </li>
+
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://github.com/laravel/laravel">GitHub</a></li>
+		<li class="divider"></li>
+		<li><a href="https://laracasts.com/discuss">Forums</a></li>
+		<li><a href="http://larachat.co">Slack</a></li>
+		<li class="divider"></li>
+		<li><a href="https://laravel-news.com/">Newsletter</a></li>
+		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
 		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
-		<li class="divider"></li>
-		<li><a href="https://laracasts.com/discuss">Laracasts Forums</a></li>
-		<li><a href="http://laravel.io/forum">Laravel.io Forums</a></li>
-		<li><a href="http://larachat.co">LaraChat Slack Channel</a></li>
-		<li><a href="https://laravel-news.com/">Laravel News</a></li>
-		<li><a href="http://www.laravelpodcast.com/">Laravel Podcast</a></li>
-		<li class="divider"></li>
-		<li><a href="http://laravelcollective.com">Laravel Collective</a></li>
-		<li class="divider"></li>
-		<li><a href="https://larajobs.com/?partner=5#/">LaraJobs</a></li>
 	</ul>
 </li>
