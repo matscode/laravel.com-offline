79c04027afc6ee23c7df857a1eeae8755b68da1e
Taylor Otwell <taylorotwell@gmail.com>
2015-12-22 13:31:01 -0600

    move links

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -25,12 +25,11 @@
 		<li class="divider"></li>
 		<li><a href="http://blog.laravel.com">Laravel Blog</a></li>
 		<li class="divider"></li>
-		<li><a href="https://laracasts.com/discuss">Forums</a></li>
-		<li><a href="http://larachat.co">Slack</a></li>
-		<li class="divider"></li>
 		<li><a href="https://larajobs.com/?partner=5#/">Jobs</a></li>
+		<li><a href="https://laracasts.com/discuss">Forums</a></li>
 		<li><a href="https://laravel-news.com/">Newsletter</a></li>
 		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
+		<li><a href="http://larachat.co">Slack</a></li>
 		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
 	</ul>
 </li>
