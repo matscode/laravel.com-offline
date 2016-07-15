07827915384dc2378d1e21d1fd6172ceb466ecca
Joseph Silber <contact@josephsilber.com>
2016-07-14 22:14:41 -0400

    Delete the Inspire command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,8 +11,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\Inspire',
-        'App\Console\Commands\IndexDocumentation',
+        Commands\IndexDocumentation::class,
     ];
 
     /**
