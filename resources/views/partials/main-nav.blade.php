d1d527a1bcbc171efd6dbb9478365335d92ee15b
Taylor Otwell <taylorotwell@gmail.com>
2016-07-29 00:15:50 -0400

    wip

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,13 +1,14 @@
 <li class="nav-docs"><a href="/docs">Documentation</a></li>
 <li class="nav-laracasts"><a href="https://laracasts.com">Laracasts</a></li>
-<li class="nav-spark"><a href="https://spark.laravel.com">Spark</a></li>
-<li class="nav-lumen"><a href="https://lumen.laravel.com">Lumen</a></li>
+<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
+<li class="nav-laravel-news"><a href="https://laravel-news.com">News</a></li>
 
 <li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Services <span class="caret"></span></a>
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Ecosystem <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
 		<li class="nav-envoyer"><a href="https://envoyer.io">Envoyer</a></li>
+		<li class="nav-lumen"><a href="https://lumen.laravel.com">Lumen</a></li>
+		<li class="nav-spark"><a href="https://spark.laravel.com">Spark</a></li>
 	</ul>
 </li>
 
@@ -24,11 +25,8 @@
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://github.com/laravel/laravel">GitHub</a></li>
 		<li class="divider"></li>
-		<li><a href="http://blog.laravel.com">Laravel Blog</a></li>
-		<li class="divider"></li>
 		<li><a href="https://larajobs.com/?partner=5#/">Jobs</a></li>
 		<li><a href="https://laracasts.com/discuss">Forums</a></li>
-		<li><a href="https://laravel-news.com/">Newsletter</a></li>
 		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
 		<li><a href="http://larachat.co">Slack</a></li>
 		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
