8430123b57b41aa557bb162afa0b1460719e7fea
Joseph Silber <contact@josephsilber.com>
2016-08-10 11:09:27 -0400

    Update build. Use local composer for Sami

--- build/docs.sh
+++ build/docs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cd /home/forge/laravel.com/resources/docs/5.0 && git pull origin 5.0
-cd /home/forge/laravel.com/resources/docs/5.1 && git pull origin 5.1
-cd /home/forge/laravel.com/resources/docs/5.2 && git pull origin 5.2
-cd /home/forge/laravel.com/resources/docs/5.3 && git pull origin 5.3
-cd /home/forge/laravel.com/resources/docs/master && git pull origin master
-cd /home/forge/laravel.com && php artisan docs:clear-cache
+base=/home/forge/laravel.com
+docs=${base}/resources/docs
+
+cd ${docs}/5.0 && git pull origin 5.0
+cd ${docs}/5.1 && git pull origin 5.1
+cd ${docs}/5.2 && git pull origin 5.2
+cd ${docs}/5.3 && git pull origin 5.3
+cd ${docs}/master && git pull origin master
+
+cd $base && php artisan docs:clear-cache
