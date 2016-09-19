e09e65c0c3399b05c08902b40261a4788af01761
Ben Sampson <bbashy@users.noreply.github.com>
2016-09-19 11:24:04 +0100

    Lavarel -> Laravel
    Not sure if this was intentional but we'll see :)

--- resources/views/partials/algolia_template.php
+++ resources/views/partials/algolia_template.php
@@ -42,7 +42,7 @@
 
 <script id="search_footer_template" type="text/template">
 	<div class="footer">
-		<a target="_blank" href="http://www.algolia.com/?utm_source=lavarel&utm_medium=link&utm_campaign=lavarel_documentation_search">
+		<a target="_blank" href="http://www.algolia.com/?utm_source=laravel&utm_medium=link&utm_campaign=laravel_documentation_search">
 			<img width="50" src="/assets/img/algolia-logo.png">
 			<div class="powered">powered by</div>
 			<div style="clear: both"></div>
