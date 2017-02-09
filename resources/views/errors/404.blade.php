f52befa146aed568a771d54483d529229b9036bc
markacianfrani <markacianf@gmail.com>
2017-02-08 20:24:10 -0500

    added missing slide menu to 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,20 @@ the-404
 @endsection
 
 @section('content')
-	
+	<nav id="slide-menu" class="slide-menu" role="navigation">
+
+        <div class="brand">
+            <a href="/">
+                <img src="/assets/img/laravel-logo-white.png" height="50" alt="Laravel white logo">
+            </a>
+        </div>
+
+        <ul class="slide-main-nav">
+            @include('partials.main-nav')
+        </ul>
+
+    </nav>
+
 	<div class="contain">
 		<div class="media">
 			<img src="/assets/img/lamp-post.jpg">
