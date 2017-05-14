67b5e1e81964f98a37524a8de2f98807bc9a890b
Owen <owen@selesti.com>
2017-05-14 20:48:16 +0100

    updated to jquery 3.2.1 - IE9+ support

--- resources/assets/js/laravel.js
+++ resources/assets/js/laravel.js
@@ -27,7 +27,7 @@ new Vue({
 
 jQuery(function($) {
   // Smooth scroll to anchor
-  $('body.home a[href*=#]:not([href=#])').click(function() {
+  $('body.home a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
