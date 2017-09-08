2f67e136d9966b254576b0e79894d1ac8d193afe
Ben Abbott <ben@benabbott.nz>
2017-09-08 16:42:19 +1200

    larachat redirects to https

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -24,7 +24,7 @@
 		<li><a href="https://github.com/laravel/laravel">GitHub</a></li>
 		<li><a href="https://larajobs.com/?partner=5#/">Jobs</a></li>
 		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
-		<li><a href="http://larachat.co">Slack</a></li>
+		<li><a href="https://larachat.co">Slack</a></li>
 		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
 	</ul>
 </li>
