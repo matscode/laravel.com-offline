c4aa2add9f8157da68131b664165bb6f70eea6d8
Steve Schoger <sm.schoger@gmail.com>
2018-01-16 09:42:02 -0500

    use whitney fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,7 @@
     <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Source+Sans+Pro:200,400,700,600,400italic,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ elixir('assets/css/laravel.css') }}">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/7737514/7707592/css/fonts.css" />
     <link rel="apple-touch-icon" href="/favicon.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
 </head>
