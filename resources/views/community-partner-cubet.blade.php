9afcb1c1fed01971d99bf0c762ba67a9ecb4783a
Taylor Otwell <taylor@laravel.com>
2018-02-07 15:32:45 -0600

    cubet

--- resources/views/community-partner-cubet.blade.php
+++ resources/views/community-partner-cubet.blade.php
@@ -25,7 +25,7 @@
                 <?php echo file_get_contents(public_path('assets/img/partner-img-cubet-logo.svg')); ?>
             </div>
             <div class="partner-ctas">
-                <a href="mailto:sales@cubettech.com"><div class="btn btn-primary">Hire cubet</div></a>
+                <a href="mailto:sales@cubettech.com"><div class="btn btn-primary">Hire Cubet</div></a>
                 <a href="https://cubettech.com/"><div class="btn btn-default">Visit Website</div></a>
             </div>
         </div>
@@ -39,8 +39,8 @@
                     <div class="flex">
                         <ul class="partner-proficiencies-list multi-col flex-fill">
                             <li>Laravel Development</li>
-                            <li>Vue/Angular/React/Node</li>
-                            <li>UI/UX Product Design</li>
+                            <li>Vue / Angular / React</li>
+                            <li>UI / UX Product Design</li>
                             <li>API Design</li>
                             <li>Software Architecture</li>
                         </ul>
@@ -66,11 +66,11 @@
             </div>
 
             <div class="partner-profile-bio col-md-6">
-                <h2>About cubet</h2>
+                <h2>About Cubet</h2>
                 <p>Cubet Techno Labs is a 150+ member Digital Engineering company with offices in India and London, helping to deliver your digital dreams to perfection. Formed in 2007, Cubet has been focused on SMAC (Social/Mobile/Analytics/Cloud) development and consulting to help enterprises and online businesses go from idea to execution. </p>
                 <p>Our extensive Laravel development services are perfect for highly agile business methodologies. Our certified Laravel developers create brilliant extensions and accurately map web applications to ensure faster, optimal performance of your solutions. With a clear focus on creating optimized database integrations that reduce lag and increase performance, our Laravel development services provide speed, agility and performance.</p>
                 <p>We always place great emphasis on FOSS (Free, Open-Source Software) front end and back end technologies including JavaScript Frameworks like Vue/Node/Angular/React/Meteor and Mobile app technologies- iOS/Android/React Native as well as cloud deployment on AWS. There is a dedicated team of UI/UX/HTML developers, Project Management and QA experts to support all projects and ensure bug free releases, all working alongside our London based account management teams.</p>
-                <p>We have over 30+ portfolio of enterprise mobile/web application projects built on the Laravel framework and we would be happy to talk with you about the enormous possibilities of using it for your next idea - however big or small. Some of our clients include enterprises like The British Medical Journal as well innovative Tech Start-ups like Active Inspiration from the UK, US, EU & Australia</p>
+                <p>We have over 30+ portfolio of enterprise mobile/web application projects built on the Laravel framework and we would be happy to talk with you about the enormous possibilities of using it for your next idea - however big or small. Some of our clients include enterprises like The British Medical Journal as well innovative Tech Start-ups like Active Inspiration from the UK, US, EU & Australia.</p>
             </div>
         </div>
     </div>
